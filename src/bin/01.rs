a43356c057b5de46b1e9c1e441f455ac742c0a31
Calum <calum@calumm.dev>
2026-04-21 22:02:31 +0100

    feat(day1): solve part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,194 @@
+use std::str::FromStr;
+
+advent_of_code::solution!(1);
+
+/// Dial position must be between 0-99 inclusive, and wrap
+/// I.E, 99 + 2 = 1
+#[derive(Debug, Copy, Clone)]
+struct Dial(usize);
+
+impl Default for Dial {
+    fn default() -> Self {
+        Dial(50)
+    }
+}
+
+impl PartialEq<usize> for Dial {
+    fn eq(&self, other: &usize) -> bool {
+        self.0 == *other
+    }
+}
+
+impl Dial {
+    const MAX: usize = 99;
+
+    fn sub(&mut self, rhs: usize) {
+        let (mut result, overflow) = self.0.overflowing_sub(rhs % (Self::MAX + 1));
+
+        if overflow {
+            result = Self::MAX - (usize::MAX - result);
+        }
+
+        self.0 = result;
+    }
+
+    fn add(&mut self, rhs: usize) {
+        let mut result = self.0 + (rhs % (Self::MAX + 1));
+
+        if Self::MAX < result {
+            result = (result - Self::MAX).saturating_sub(1);
+        }
+
+        self.0 = result;
+    }
+
+    fn apply(&mut self, direction: Direction, distance: usize) {
+        match direction {
+            Direction::Left => self.sub(distance),
+            Direction::Right => self.add(distance),
+        }
+    }
+}
+
+enum Direction {
+    Left,
+    Right,
+}
+
+impl From<char> for Direction {
+    fn from(c: char) -> Self {
+        match c {
+            'L' => Direction::Left,
+            'R' => Direction::Right,
+            _ => unimplemented!(),
+        }
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut zero_count = 0;
+    let mut dial = Dial::default();
+
+    for line in input.lines() {
+        let direction = Direction::from(line.as_bytes()[0] as char);
+        let distance = usize::from_str(&line[1..]).unwrap();
+
+        dial.apply(direction, distance);
+        if dial == 0 {
+            zero_count += 1;
+        }
+    }
+
+    Some(zero_count)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn test_dial_sub() {
+        {
+            let mut dial = Dial(50);
+            dial.sub(68);
+            assert_eq!(dial, 82);
+        }
+
+        {
+            let mut dial = Dial(82);
+            dial.sub(30);
+            assert_eq!(dial, 52);
+        }
+
+        {
+            let mut dial = Dial(0);
+            dial.sub(5);
+            assert_eq!(dial, 95);
+        }
+
+        {
+            let mut dial = Dial(55);
+            dial.sub(55);
+            assert_eq!(dial, 0);
+        }
+
+        {
+            let mut dial = Dial(0);
+            dial.sub(1);
+            assert_eq!(dial, 99);
+        }
+
+        {
+            let mut dial = Dial(99);
+            dial.sub(99);
+            assert_eq!(dial, 0);
+        }
+
+        {
+            let mut dial = Dial(14);
+            dial.sub(82);
+            assert_eq!(dial, 32);
+        }
+
+        {
+            let mut dial = Dial(14);
+            dial.sub(105);
+            assert_eq!(dial, 9);
+
+            dial.sub(337);
+            assert_eq!(dial, 72);
+        }
+    }
+
+    #[test]
+    fn test_dial_add() {
+        {
+            let mut dial = Dial(52);
+            dial.add(48);
+            assert_eq!(dial, 0);
+        }
+
+        {
+            let mut dial = Dial(95);
+            dial.add(60);
+            assert_eq!(dial, 55);
+        }
+
+        {
+            let mut dial = Dial(0);
+            dial.add(14);
+            assert_eq!(dial, 14);
+        }
+
+        {
+            let mut dial = Dial(0);
+            dial.add(14);
+            assert_eq!(dial, 14);
+        }
+
+        {
+            let mut dial = Dial(0);
+            dial.add(140);
+            assert_eq!(dial, 40);
+
+            dial.add(351);
+            assert_eq!(dial, 91);
+        }
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
