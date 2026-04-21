e31b50ea46b2ba63a3f8f1aa2158baaf1449836b
Calum <calum@calumm.dev>
2026-04-21 22:42:45 +0100

    feat(day1): solve part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -22,27 +22,51 @@ impl PartialEq<usize> for Dial {
 impl Dial {
     const MAX: usize = 99;
 
-    fn sub(&mut self, rhs: usize) {
+    /// # Returns
+    /// Number of times the dial passed zero
+    fn sub(&mut self, rhs: usize) -> u64 {
+        let mut passed_zero = (rhs / (Self::MAX + 1)) as u64;
+        let started_at_zero = self.0 == 0;
+
         let (mut result, overflow) = self.0.overflowing_sub(rhs % (Self::MAX + 1));
 
         if overflow {
             result = Self::MAX - (usize::MAX - result);
+            passed_zero += 1;
+        }
+
+        if result == 0 {
+            passed_zero += 1;
         }
 
         self.0 = result;
+
+        if started_at_zero {
+            passed_zero = passed_zero.saturating_sub(1);
+        }
+
+        passed_zero
     }
 
-    fn add(&mut self, rhs: usize) {
+    /// # Returns
+    /// Number of times the dial passed zero
+    fn add(&mut self, rhs: usize) -> u64 {
+        let mut passed_zero = (rhs / (Self::MAX + 1)) as u64;
         let mut result = self.0 + (rhs % (Self::MAX + 1));
 
         if Self::MAX < result {
             result = (result - Self::MAX).saturating_sub(1);
+            passed_zero += 1;
         }
 
         self.0 = result;
+
+        passed_zero
     }
 
-    fn apply(&mut self, direction: Direction, distance: usize) {
+    /// # Returns
+    /// Number of times the dial passed zero
+    fn apply(&mut self, direction: Direction, distance: usize) -> u64 {
         match direction {
             Direction::Left => self.sub(distance),
             Direction::Right => self.add(distance),
@@ -83,7 +107,17 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut zero_count = 0;
+    let mut dial = Dial::default();
+
+    for line in input.lines() {
+        let direction = Direction::from(line.as_bytes()[0] as char);
+        let distance = usize::from_str(&line[1..]).unwrap();
+
+        zero_count += dial.apply(direction, distance);
+    }
+
+    Some(zero_count)
 }
 
 #[cfg(test)]
@@ -97,55 +131,61 @@ mod tests {
     }
 
     #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6));
+    }
+
+    #[test]
     fn test_dial_sub() {
         {
             let mut dial = Dial(50);
-            dial.sub(68);
+            assert_eq!(dial.sub(68), 1);
             assert_eq!(dial, 82);
         }
 
         {
             let mut dial = Dial(82);
-            dial.sub(30);
+            assert_eq!(dial.sub(30), 0);
             assert_eq!(dial, 52);
         }
 
         {
             let mut dial = Dial(0);
-            dial.sub(5);
+            assert_eq!(dial.sub(5), 0);
             assert_eq!(dial, 95);
         }
 
         {
             let mut dial = Dial(55);
-            dial.sub(55);
+            assert_eq!(dial.sub(55), 1);
             assert_eq!(dial, 0);
         }
 
         {
             let mut dial = Dial(0);
-            dial.sub(1);
+            assert_eq!(dial.sub(1), 0);
             assert_eq!(dial, 99);
         }
 
         {
             let mut dial = Dial(99);
-            dial.sub(99);
+            assert_eq!(dial.sub(99), 1);
             assert_eq!(dial, 0);
         }
 
         {
             let mut dial = Dial(14);
-            dial.sub(82);
+            assert_eq!(dial.sub(82), 1);
             assert_eq!(dial, 32);
         }
 
         {
             let mut dial = Dial(14);
-            dial.sub(105);
+            assert_eq!(dial.sub(105), 1);
             assert_eq!(dial, 9);
 
-            dial.sub(337);
+            assert_eq!(dial.sub(337), 4);
             assert_eq!(dial, 72);
         }
     }
@@ -154,41 +194,32 @@ mod tests {
     fn test_dial_add() {
         {
             let mut dial = Dial(52);
-            dial.add(48);
+            assert_eq!(dial.add(48), 1);
             assert_eq!(dial, 0);
         }
 
         {
             let mut dial = Dial(95);
-            dial.add(60);
+            assert_eq!(dial.add(60), 1);
             assert_eq!(dial, 55);
         }
 
         {
             let mut dial = Dial(0);
-            dial.add(14);
+            assert_eq!(dial.add(14), 0);
             assert_eq!(dial, 14);
         }
 
         {
             let mut dial = Dial(0);
-            dial.add(14);
-            assert_eq!(dial, 14);
-        }
-
-        {
-            let mut dial = Dial(0);
-            dial.add(140);
+            assert_eq!(dial.add(140), 1);
             assert_eq!(dial, 40);
 
-            dial.add(351);
+            assert_eq!(dial.add(351), 3);
             assert_eq!(dial, 91);
-        }
-    }
 
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+            assert_eq!(dial.add(110), 2);
+            assert_eq!(dial, 1);
+        }
     }
 }
