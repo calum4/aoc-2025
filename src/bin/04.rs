720d9e396d02630b2f335e647af8d8fabbfd64ec
Calum <calum@calumm.dev>
2026-04-25 22:36:28 +0100

    perf(day4): calculate rolling x+-1 total on input parse

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -12,56 +12,70 @@ struct Coordinate {
 }
 
 impl Coordinate {
-    fn adjacent_coords(&self) -> [Option<Coordinate>; 8] {
-        let mut adjacent = [None; 8];
+    fn adjacent_coords(&self) -> [Option<Coordinate>; 3] {
+        let mut adjacent = [None; 3];
+
         let mut index = 0;
 
-        // (x|y) <= 139
-        let cur_y = self.y as i16;
-        let cur_x = self.x as i16;
+        {
+            let y = self.y + 1;
 
-        for y_offset in [-1, 0, 1] {
-            let y = cur_y + y_offset;
-
-            if y < 0 || LEN as i16 <= y {
-                continue;
-            }
-
-            for x_offset in [-1, 0, 1] {
-                if y_offset == 0 && x_offset == 0 {
-                    continue;
-                }
-
-                let x = cur_x + x_offset;
-
-                if x < 0 || LEN as i16 <= x {
-                    continue;
-                }
-
-                adjacent[index] = Some(Coordinate {
-                    x: x as usize,
-                    y: y as usize,
-                });
+            if y < LEN {
+                adjacent[index] = Some(Coordinate { x: self.x, y });
 
                 index += 1;
             }
+        }
+
+        adjacent[index] = Some(Coordinate {
+            x: self.x,
+            y: self.y,
+        });
+        index += 1;
+
+        if let Some(y) = self.y.checked_sub(1) {
+            adjacent[index] = Some(Coordinate { x: self.x, y });
         }
 
         adjacent
     }
 }
 
-fn parse_input(input: &str) -> ([[bool; LEN]; LEN], Vec<Coordinate>) {
-    let mut grid = [[false; LEN]; LEN];
+fn parse_row(y: usize, line: &str, to_check: &mut Vec<Coordinate>) -> [(bool, u8); LEN] {
+    let mut row = [(false, 0); LEN];
+
+    for (x, elem) in line.chars().enumerate() {
+        let hit = elem == '@';
+        let mut cur_count = 0;
+
+        if let Some(prev) = x.checked_sub(1).map(|prev_x| &mut row[prev_x]) {
+            if hit {
+                prev.1 += 1;
+            }
+
+            if prev.0 {
+                cur_count += 1;
+            }
+        }
+
+        if hit {
+            cur_count += 1;
+            to_check.push(Coordinate { x, y });
+        }
+
+        row[x] = (hit, cur_count);
+    }
+
+    row
+}
+
+// TODO - Store column major to optimise element access
+fn parse_input(input: &str) -> ([[(bool, u8); LEN]; LEN], Vec<Coordinate>) {
+    let mut grid = [[(false, 0); LEN]; LEN];
     let mut to_check = Vec::with_capacity(LEN * LEN);
 
     input.lines().rev().enumerate().for_each(|(y, line)| {
-        for (x, elem) in line.chars().enumerate() {
-            if elem == '@' {
-                grid[y][x] = true;
-                to_check.push(Coordinate { x, y });
-            }
-        }
+        grid[y] = parse_row(y, line, &mut to_check);
     });
 
     (grid, to_check)
@@ -76,14 +90,13 @@ pub fn part_one(input: &str) -> Option<u64> {
 
         let mut coords = coordinate.adjacent_coords().into_iter();
         while let Some(Some(coord)) = coords.next()
-            && total < 4
+            && total < 5
         {
-            if grid[coord.y][coord.x] {
-                total += 1;
-            }
+            total += grid[coord.y][coord.x].1;
         }
 
-        if total < 4 {
+        // Less than 5 because the coordinate origin (not adjacent) is counted in the total
+        if total < 5 {
             result += 1;
         }
     }
